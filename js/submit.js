d86eb96a4ee046faf3272b12ff8de998e3f65183
ayelenleclerc <ayelenleclerc@gmail.com>
2021-12-13 19:25:18 -0300

    Curriculum casi terminado

---
+++ js/submit.js
@@ -0,0 +1,19 @@
+const $form = document.querySelector("#form");
+
+$form.addEventListener("submit", envioMail);
+
+async function envioMail(event) {
+  event.preventDefault();
+  const form = new FormData(this);
+  const response = await fetch(this.action, {
+    method: this.method,
+    body: form,
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  if (response.ok) {
+    this.reset();
+    alert("¡Gracias por contactarme!");
+  }
+}
